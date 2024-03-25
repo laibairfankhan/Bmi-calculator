ec21f16baa68339ca05c2349ceb4d8c4c887930c
laibairfankhan <irfanlaiba978@gmail.com>
2024-03-25 09:27:41 -0700

    push my bmi calculator to github

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+
+import chalk from "chalk";
+const userAnswer = await inquirer.prompt([
+    {
+        message:  "Enter your weight in kg",
+        type: "number",
+        name: "weight",
+
+    },
+    {
+        message: "Enter your height in meters",
+        type: "number",
+        name: "height",
+    },
+])
+    const weight = userAnswer.weight;
+    const height = userAnswer.height;
+    const BMI = userAnswer.weight / (userAnswer.height * userAnswer.height);
+    console.log("Your BMI is:", chalk.bgCyanBright(BMI));
+    if(BMI < 18.5){
+        console.log(chalk.bgBlueBright("you are under weight"));
+    }else if(BMI < 24){
+        console.log(chalk.bgGreenBright("you are normal weight"));
+    }else if(BMI < 30){
+        console.log(chalk.bgRedBright("you are over weight"));
+    }else(chalk.bgCyanBright("you are obese"));
+
+
+    
+
